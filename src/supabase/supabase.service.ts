5d6073deba2870be405249a1a16f8d9d20e63979
shyam-ga04 <shyamganeshravichandran4@gmail.com>
2025-12-06 20:57:17 +0530

    feat: the microservice, auth module and supbase setup done.

---
+++ src/supabase/supabase.service.ts
@@ -0,0 +1,19 @@
+
+import { Injectable } from '@nestjs/common';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class SupabaseService {
+  private supabase: SupabaseClient;
+
+  constructor(private configService: ConfigService) {
+    const supabaseUrl = this.configService.get<string>('SUPABASE_URL');
+    const supabaseKey = this.configService.get<string>('SUPABASE_KEY');
+    this.supabase = createClient(supabaseUrl, supabaseKey);
+  }
+
+  getClient(): SupabaseClient {
+    return this.supabase;
+  }
+}
